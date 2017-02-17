da4cbbb69c75094799af5bad5551ce2b223378c1
Usman Pervaiz <de.usman@yahoo.com>
2017-02-17 23:50:51 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
 # Add  code here!
+def prime?(number)
+  if number <= 2
+    return false
+  end
+  for checker in 2...number do
+    if (number % checker) == 0
+    return false
+    end
+  end
+   true
+end
